ed52a46516274ff429a3c5e4d02f953f180f5d63
Vadim Kamalov <vvkamalov_obs@mail.ru>
2025-05-09 18:45:37 +0500

    init document-management-service

---
+++ document-management-service/src/main/java/com/github/tennyros/management/DocumentManagementService.java
@@ -0,0 +1,11 @@
+package com.github.tennyros.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementService {
+    public static void main( String[] args ) {
+        SpringApplication.run(DocumentManagementService.class, args);
+    }
+}
