087ab66296d2561b236e93098ac8aa4a51f02c4e
Vadim Kamalov <vvkamalov_obs@mail.ru>
2025-05-11 21:33:02 +0500

    DP6: add exceptions, controller advice, exception catch logic in MinioService, change download response contentType

---
+++ document-management-service/src/main/java/com/github/tennyros/management/exception/FileNotFoundException.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.management.exception;
+
+public class FileNotFoundException extends RuntimeException {
+
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+
+    public FileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
