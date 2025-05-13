740fa561fdd10cb69a74837bfa19d090b90c3c5f
Vadim Kamalov <vvkamalov_obs@mail.ru>
2025-05-13 18:39:27 +0500

    DP9: add DocumentMetadataService, DocumentMapper, extra method to DocumentVersionRepository

--- document-management-service/src/main/java/com/github/tennyros/management/repository/DocumentVersionRepository.java
+++ document-management-service/src/main/java/com/github/tennyros/management/repository/DocumentVersionRepository.java
@@ -3,5 +3,11 @@ package com.github.tennyros.management.repository;
 import com.github.tennyros.management.model.DocumentVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DocumentVersionRepository extends JpaRepository<DocumentVersion, Long> {
+
+    Optional<DocumentVersion> findByStorageKey(String storageKey);
+
+    boolean existsByStorageKey(String storageKey);
 }
