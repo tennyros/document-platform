a82fe133e4319ab49692748fb55d1fd1a5b79f31
Vadim Kamalov <vvkamalov_obs@mail.ru>
2025-05-18 23:35:12 +0500

    dto: add DocumentFilter request, Document and Page responses

---
+++ document-management-service/src/main/java/com/github/tennyros/management/dto/response/DocumentResponse.java
@@ -0,0 +1,24 @@
+package com.github.tennyros.management.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+
+}
