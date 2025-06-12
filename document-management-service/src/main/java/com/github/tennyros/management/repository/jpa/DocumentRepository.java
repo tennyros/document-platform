771afd74a7104ac1cbac78998392c103ad15379e
Vadim Kamalov <vvkamalov_obs@mail.ru>
2025-06-12 18:13:40 +0500

    feat: add liquibase migrations, code refactor
    1. Add libuibase xml migrations, maven liquibase plugin in DMS, liquibase.properties file.
    2. Change .env.sample credentials, add Dotenv variables loading in DMS, .env to DMS Dockerfile for variables include.
    3. Add mongock dependencies to pom.xml for mongo db migrations.
    4. Add RepositoryConfig class with separated postgresql and mongo dbs loading.
    5. Refactor DMS application.yml files according to previous changes.
    6. Add DSS service in docker-compose file.
    7. Chore readmes.

--- document-management-service/src/main/java/com/github/tennyros/management/repository/jpa/DocumentRepository.java
+++ document-management-service/src/main/java/com/github/tennyros/management/repository/jpa/DocumentRepository.java
@@ -3,11 +3,12 @@ package com.github.tennyros.management.repository;
 import com.github.tennyros.management.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DocumentRepository extends JpaRepository<Document, Long>, JpaSpecificationExecutor<Document> {
 
     Optional<Document> findByTitle(String title);
-
 }
